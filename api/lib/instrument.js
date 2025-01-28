a7b7651adba77dc325d460d2caf7fdf6b9f09205
Julian Gruber <julian@juliangruber.com>
2025-01-28 11:58:38 +0100

    fix sentry fastify error reporting

--- api/lib/instrument.js
+++ api/lib/instrument.js
@@ -21,5 +21,16 @@ Sentry.init({
   dsn: 'https://01b72a603b1bd4f9f821348152486dc2@o1408530.ingest.us.sentry.io/4508002559393792',
   release: pkg.version,
   environment: SENTRY_ENVIRONMENT,
-  tracesSampleRate: 0.1
+  tracesSampleRate: 0.1,
+  // Ignore Fastify 4xx errors
+  // Remove once https://github.com/getsentry/sentry-javascript/pull/13198 lands
+  beforeSend (event, { originalException: err }) {
+    const isBadRequest =
+      typeof err === 'object' &&
+      err !== null &&
+      'statusCode' in err &&
+      typeof err.statusCode === 'number' &&
+      err.statusCode < 500
+    return isBadRequest ? null : event
+  }
 })
