1338e59913b28e2b08a42cdec220334233e2b988
Nikolas Haimerl <nikolas@meridian.space>
2025-03-11 14:23:25 +0100

    comments

--- indexer/test/helpers/test-data.js
+++ indexer/test/helpers/test-data.js
@@ -1,5 +1,7 @@
 // See https://github.com/filecoin-station/frisbii-on-fly
 export const FRISBII_ID = '12D3KooWC8gXxg9LoJ9h3hy3jzBkEAxamyHEQJKtRmAuBuvoMzpr'
 export const FRISBII_ADDRESS = 'https://frisbii.fly.dev'
-// This CID may change when Frisbii is redeployed. You can find the latest CID in the Frisbii repo: https://github.com/CheckerNetwork/frisbii-on-fly/blob/178c3949619d5b6e9c761d4a5343dbe9c93f457c/README.md
+// This CID may change when Frisbii is redeployed. You can find the CIDs of the advertised data in this repo: https://github.com/CheckerNetwork/frisbii-on-fly/blob/178c3949619d5b6e9c761d4a5343dbe9c93f457c/README.md
+// The advertisement CID itselve can be retrieved by querying the provider ID (FRISBII_ID):  https://cid.contact/providers/12D3KooWC8gXxg9LoJ9h3hy3jzBkEAxamyHEQJKtRmAuBuvoMzpr
+// The LastAdvertisement field is the CID of the advertisement (FRISBII_AD_CID).
 export const FRISBII_AD_CID = 'baguqeerafqzjc6vyefzivmcvcex77eylhiipvkaa3wcmn6vxulihillwfloq'
