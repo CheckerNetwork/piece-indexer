0d964686c35f93978ed623c5772a6bc586adc5d3
Nikolas Haimerl <nikolas@meridian.space>
2025-03-11 14:21:20 +0100

    advertisement cid

--- indexer/test/helpers/test-data.js
+++ indexer/test/helpers/test-data.js
@@ -2,4 +2,4 @@
 export const FRISBII_ID = '12D3KooWC8gXxg9LoJ9h3hy3jzBkEAxamyHEQJKtRmAuBuvoMzpr'
 export const FRISBII_ADDRESS = 'https://frisbii.fly.dev'
 // This CID may change when Frisbii is redeployed. You can find the latest CID in the Frisbii repo: https://github.com/CheckerNetwork/frisbii-on-fly/blob/178c3949619d5b6e9c761d4a5343dbe9c93f457c/README.md
-export const FRISBII_AD_CID = 'bafkreih25dih6ug3xtj73vswccw423b56ilrwmnos4cbwhrceudopdp5sq'
+export const FRISBII_AD_CID = 'baguqeerafqzjc6vyefzivmcvcex77eylhiipvkaa3wcmn6vxulihillwfloq'
