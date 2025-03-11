f168b91c6152b2bd5cc21e82f692b5ff311c1158
Nikolas Haimerl <nikolas@meridian.space>
2025-03-11 14:00:41 +0100

    fix frisbii cid

--- indexer/test/helpers/test-data.js
+++ indexer/test/helpers/test-data.js
@@ -1,4 +1,5 @@
 // See https://github.com/filecoin-station/frisbii-on-fly
 export const FRISBII_ID = '12D3KooWC8gXxg9LoJ9h3hy3jzBkEAxamyHEQJKtRmAuBuvoMzpr'
 export const FRISBII_ADDRESS = 'https://frisbii.fly.dev'
-export const FRISBII_AD_CID = 'baguqeeraacf25e7glnmzgtocthwavkhq7z7jrv576aofwg3jr5mn3qqomp5a'
+// This CID may change when Frisbii is redeployed. You can find the latest CID in the Frisbii repo: https://github.com/CheckerNetwork/frisbii-on-fly?tab=readme-ov-file#lorem-ipsum
+export const FRISBII_AD_CID = 'bafkreih25dih6ug3xtj73vswccw423b56ilrwmnos4cbwhrceudopdp5sq'
